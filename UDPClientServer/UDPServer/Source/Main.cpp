3d9aafb0bf83c45b5246431c63c14c45e815a6c7
UtilitasArts <Utilitasarts@gmail.com>
2024-12-13 16:40:35 +0100

    update

--- UDPClientServer/UDPServer/Source/Main.cpp
+++ UDPClientServer/UDPServer/Source/Main.cpp
@@ -64,7 +64,7 @@ void UpdateServer() {
 	fs::path Location = fs::current_path();
 	std::cout << "\n" << Location.string() << "\n";
 
-	std::string Command = "cd \"" + Repos.string() + "\" && git status && git pull && start cmd /K \"cd " + Location.string() + "&&UDPServer \"    ";
+	std::string Command = "cd \"" + Repos.string() + "\" && git status && git pull && start cmd /K \" " + Location.string() + "UDPServer \"    ";
 	system(Command.c_str());
 
 	exit(0);
