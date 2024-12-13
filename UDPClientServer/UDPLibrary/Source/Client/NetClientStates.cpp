e9a3c09c9ce12c408bacda73fc586408c847445d
UtilitasArts <Utilitasarts@gmail.com>
2024-12-13 22:26:05 +0100

    update

--- UDPClientServer/UDPLibrary/Source/Client/NetClientStates.cpp
+++ UDPClientServer/UDPLibrary/Source/Client/NetClientStates.cpp
@@ -126,8 +126,9 @@ void Unconnected_NetClientState::OnEnter() {
 						} fs::path ReposFolder = RestartFolder.parent_path();						
 						RestartFolder = RestartFolder / "x64" / "Release" / "UDPClient"; 	
 						
-						std::string Command3 = "&& start cmd /K \"" + RestartFolder.string() + "\"";
-						std::string Command2 = "&& git add . && git commit -m \"" + CommitLog + "\" && git push -u origin main" + Command3 + "&& exit";
+						//std::string Command3 = "&& start cmd /K \"" + RestartFolder.string() + "\"";
+
+						std::string Command2 = "&& git add . && git commit -m \"" + CommitLog + "\" && git push -u origin main";
 						std::string Command  = "cd " + ReposFolder.string() + Command2 + "\" ";
 
  						system(Command.c_str());
