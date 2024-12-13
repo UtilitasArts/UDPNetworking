ebf682a1ef336a70a3960d26104957e700eb9620
UtilitasArts <Utilitasarts@gmail.com>
2024-12-14 00:01:46 +0100

    update

--- UDPClientServer/UDPServer/Source/Main.cpp
+++ UDPClientServer/UDPServer/Source/Main.cpp
@@ -78,7 +78,7 @@ void UpdateServer() {
 
 
 	std::string Command2 = "&& git pull && cls && cd \"" + UDPSetup::RestartFolder.string() + "\" && UDPServer.exe && exit";
-	std::string Command = "start cmd /K \"cd \"" + UDPSetup::ReposFolder.string() + "\"" + Command2 + "\"";
+	std::string Command = "start /MIN cmd /K \"cd \"" + UDPSetup::ReposFolder.string() + "\"" + Command2 + "\"";
 	system(Command.c_str());
 
 	std::cout << "- Update of server was approved, Restarting now";
